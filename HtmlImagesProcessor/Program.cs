9c54c1ec64a5c25a0160409298706759f41d2c69
Victor Gorokhov <me@rkit.pp.ru>
2013-04-24 09:26:09 +0300

    Начальный коммир + рабочая версия коммандлайна

---
+++ HtmlImagesProcessor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HtmlAgilityPack;
+
+namespace HtmlImagesProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlDocument doc = new HtmlDocument();
+            string p = "C:\\Users\\Victor\\AppData\\Local\\Temp\\index.htm";
+            doc.Load(p);
+            var baseU  = new Uri(p);
+            foreach (var img in doc.DocumentNode.SelectNodes("//img[@src]"))
+            {
+                var uri = new Uri(baseU, img.Attributes["src"].Value);
+                img.Attributes["src"].Value = ToDataURI.Transform(uri).ToString();
+            }
+            doc.Save(p+"1.htm", doc.StreamEncoding);
+        }
+    }
+}
---
+++ HtmlImagesProcessor/ToDataURI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HtmlImagesProcessor
+{
+    class ToDataURI
+    {
+        public static Uri Transform(Uri u)
+        {
+            if (!u.IsFile) return u;
+            try
+            {
+                var ct = getCT(Path.GetExtension(u.LocalPath));
+                var data = Convert.ToBase64String(File.ReadAllBytes(u.LocalPath));
+                return (new Uri(String.Format("data:{0};base64,{1}", ct, data)));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString() + "at ToDataURI with" + u.ToString());
+                return u;
+            }
+        }
+
+        public static string getCT(string ext)
+        {
+            switch (ext)
+            {
+                case ".jpg":
+                case ".jpeg":
+                    return "image/jpeg";
+                case ".png":
+                    return "image/png";
+                case ".gif":
+                    return "image/gif";
+                default:
+                    return "application/octet-stream";
+            }
+        }
+    }
+}
